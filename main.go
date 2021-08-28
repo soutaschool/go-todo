7510ebad009910d2692b2a6a69fbfd2a9ac5a72e
soutaschool <sasakisouta110@icloud.com>
2021-08-28 11:26:04 +0900

    ユーザー取得

--- main.go
+++ main.go
@@ -16,11 +16,14 @@ func main() {
 
 	fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
-	fmt.Println(u)
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "test@example.com"
+	// u.PassWord = "testtest"
+	// fmt.Println(u)
 
-	u.CreateUser()
+	// u.CreateUser()
+
+	u, _ := models.GetUser(1)
+	fmt.Println(u)
 }
